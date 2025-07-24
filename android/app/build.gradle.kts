aa307b991242d7abcca70c50969a29e007795576
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2025-07-24 16:40:15 +0600

    edit android ndk version (default)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.picon.picon"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion //"27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
