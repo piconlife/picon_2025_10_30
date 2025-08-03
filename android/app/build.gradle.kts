b5ad536f50ce888f6d4aeae19344a9c57276474a
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2025-08-03 16:58:21 +0600

    clean code

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,12 +24,12 @@ android {
     ndkVersion = "29.0.13846066"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -38,7 +38,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        multiDexEnabled true
+        multiDexEnabled = true
     }
 
     signingConfigs {
