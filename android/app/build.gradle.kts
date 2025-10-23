839c8d50de227cea8ef45441414c0b7511857044
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2025-10-23 09:36:59 +0600

    reduce code like pelvix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 
     defaultConfig {
         applicationId = "com.picon.picon"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
