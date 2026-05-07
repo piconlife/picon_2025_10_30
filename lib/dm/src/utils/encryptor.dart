a44bb7428a8bd0ab274fd57846378986f740408b
Md. Mohi-Uddin <mohiuddin655.bd@email.com>
2026-05-08 00:02:47 +0600

    v1

--- lib/dm/src/utils/encryptor.dart
+++ lib/dm/src/utils/encryptor.dart
@@ -6,70 +6,9 @@ import 'package:flutter/foundation.dart';
 import 'configs.dart';
 
 typedef EncryptorRequestBuilder =
-    Map<String, dynamic> Function(String request, String passcode);
+Map<String, dynamic> Function(String request, String passcode);
 
 typedef EncryptorResponseBuilder = dynamic Function(Map<String, dynamic> data);
-
-class _EncodePayload {
-  final Map<String, dynamic> data;
-  final String key;
-  final String iv;
-  final String passcode;
-  final EncryptorRequestBuilder request;
-
-  const _EncodePayload({
-    required this.data,
-    required this.key,
-    required this.iv,
-    required this.passcode,
-    required this.request,
-  });
-}
-
-class _DecodePayload {
-  final Map<String, dynamic> source;
-  final String key;
-  final String iv;
-  final EncryptorResponseBuilder response;
-
-  const _DecodePayload({
-    required this.source,
-    required this.key,
-    required this.iv,
-    required this.response,
-  });
-}
-
-Future<Map<String, dynamic>> _encode(_EncodePayload p) async {
-  try {
-    final k = crypto.Key.fromUtf8(p.key);
-    final iv = crypto.IV.fromUtf8(p.iv);
-    final en = crypto.Encrypter(crypto.AES(k, mode: crypto.AESMode.cbc));
-    final encrypted = en.encrypt(jsonEncode(p.data), iv: iv);
-    return p.request(encrypted.base64, p.passcode);
-  } catch (e, st) {
-    debugPrint('DataEncryptor.encode error: $e\n$st');
-    return {};
-  }
-}
-
-Future<Map<String, dynamic>> _decode(_DecodePayload p) async {
-  try {
-    final value = p.response(p.source);
-    if (value is String) {
-      final k = crypto.Key.fromUtf8(p.key);
-      final iv = crypto.IV.fromUtf8(p.iv);
-      final en = crypto.Encrypter(crypto.AES(k, mode: crypto.AESMode.cbc));
-      final encrypted = crypto.Encrypted.fromBase64(value);
-      final data = en.decrypt(encrypted, iv: iv);
-      return jsonDecode(data);
-    }
-    return {};
-  } catch (e, st) {
-    debugPrint('DataEncryptor.decode error: $e\n$st');
-    return {};
-  }
-}
 
 class DataEncryptor {
   final String key;
@@ -80,7 +19,6 @@ class DataEncryptor {
   final EncryptorResponseBuilder? _response;
 
   EncryptorResponseBuilder get response => _response ?? (a) => a["data"];
-
   EncryptorRequestBuilder get request => _request ?? (a, b) => {"data": a};
 
   const DataEncryptor({
@@ -92,45 +30,51 @@ class DataEncryptor {
   }) : _request = request,
        _response = response;
 
-  Future<Map<String, dynamic>> input(dynamic data) {
-    if (data is! Map<String, dynamic>) return Future.value({});
-    return compute(
-      _encode,
-      _EncodePayload(
-        data: data,
-        key: key,
-        iv: iv,
-        passcode: passcode,
-        request: request,
-      ),
-    );
+  crypto.Key get _key => crypto.Key.fromUtf8(key);
+
+  crypto.IV get _iv => crypto.IV.fromUtf8(iv);
+
+  crypto.Encrypter get _en =>
+      crypto.Encrypter(crypto.AES(_key, mode: crypto.AESMode.cbc));
+
+  Future<Map<String, dynamic>> input(dynamic data) async {
+    if (data is! Map<String, dynamic>) return {};
+    try {
+      final encrypted = _en.encrypt(jsonEncode(data), iv: _iv);
+      return request(encrypted.base64, passcode);
+    } catch (e, st) {
+      debugPrint('DataEncryptor.input error: $e\n$st');
+      return {};
+    }
   }
 
-  Future<Map<String, dynamic>> output(dynamic data) {
-    if (data is! Map<String, dynamic>) return Future.value({});
-    return compute(
-      _decode,
-      _DecodePayload(source: data, key: key, iv: iv, response: response),
-    );
+  Future<Map<String, dynamic>> output(dynamic data) async {
+    if (data is! Map<String, dynamic>) return {};
+    try {
+      final value = response(data);
+      if (value is! String) return {};
+      final encrypted = crypto.Encrypted.fromBase64(value);
+      final decoded = _en.decrypt(encrypted, iv: _iv);
+      return jsonDecode(decoded);
+    } catch (e, st) {
+      debugPrint('DataEncryptor.output error: $e\n$st');
+      return {};
+    }
   }
 
-  static String generateKey([DataByteType type = DataByteType.x16]) {
-    return DataIdGenerator.generate(type).secretKey;
-  }
+  static String generateKey([DataByteType type = DataByteType.x16]) =>
+      DataIdGenerator.generate(type).secretKey;
 
-  static String generateIV([DataByteType type = DataByteType.x8]) {
-    return DataIdGenerator.generate(type).secretIV;
-  }
+  static String generateIV([DataByteType type = DataByteType.x8]) =>
+      DataIdGenerator.generate(type).secretIV;
 }
 
 extension DataEncryptorHelper on DataEncryptor? {
   bool get isValid => this != null;
 
-  Future<Map<String, dynamic>> input(dynamic data) async {
-    return isValid ? await this!.input(data) : {};
-  }
+  Future<Map<String, dynamic>> input(dynamic data) async =>
+      isValid ? await this!.input(data) : {};
 
-  Future<Map<String, dynamic>> output(dynamic data) async {
-    return isValid ? await this!.output(data) : {};
-  }
+  Future<Map<String, dynamic>> output(dynamic data) async =>
+      isValid ? await this!.output(data) : {};
 }
