363ca76aec8109f9a630bf3409ecef26a0ca7261
Md. Mohi-Uddin <mohiuddin655.bd@email.com>
2026-05-11 22:58:38 +0600

    v2

--- lib/features/startup/views/pages/intro.dart
+++ lib/features/startup/views/pages/intro.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:in_app_navigator/route.dart';
 import 'package:in_app_translation/in_app_translation.dart';
 import 'package:picon/app/constants/app.dart';
-import 'package:picon/dm_test/remote/remote_data_test_page.dart';
 import 'package:picon/roots/widgets/appbar.dart';
 
 import '../../../../roots/widgets/column.dart';
@@ -37,7 +36,6 @@ class _IntroPageState extends State<IntroPage>
 
   @override
   Widget build(BuildContext context) {
-    return RemoteDataTestPage();
     return StartupScreen(
       child: Scaffold(
         backgroundColor: Colors.transparent,
